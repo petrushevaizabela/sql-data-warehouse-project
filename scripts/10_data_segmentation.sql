ca1dac7949cea0c216871a771609287d49d6072b
petrushevaizabela <petrushevaizabela@gmail.com>
2025-11-23 23:18:34 +0100

    Create 10_data_segmentation.sql

---
+++ scripts/10_data_segmentation.sql
@@ -0,0 +1,74 @@
+-- Segment products into cost ranges and count how many products fall into each segment.
+
+WITH product_segments AS (
+SELECT
+	product_key,
+	product_name,
+	cost,
+	CASE
+		WHEN cost < 100 THEN 'Below 100'
+		WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+		WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+		ELSE 'Above 1000'
+	END AS cost_range
+FROM gold.dim_products
+)
+
+-- Baraa code
+SELECT
+	cost_range,
+	COUNT(product_key) AS total_products
+FROM product_segments
+GROUP BY cost_range
+ORDER BY total_products DESC
+
+/* Group customers into three segments based on their spending behaviour:
+	- VIP: Customer with at least 12 months of history and spending more than $5,000.
+	- Regular: Customers with at least 12 months of history but spending $5,000 or less.
+	- New: Customers with a lifespan less than 12 months.
+And find the total number of custmers by each group.
+*/
+
+-- Baraa code
+
+WITH customer_spending AS (
+SELECT
+	c.customer_key,
+	SUM(f.sales_amount) AS total_spending,
+	MIN(order_date) AS first_order,
+	MAX(order_date) AS last_order,
+	DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
+FROM gold.fact_sales AS f
+LEFT JOIN gold.dim_customers AS c
+ON f.customer_key = c.customer_key
+GROUP BY c.customer_key
+)
+
+-- Displaying customers and their status
+/*
+SELECT
+	customer_key,
+	total_spending,
+	lifespan,
+	CASE WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+		WHEN lifespan >= 12 AND total_spending < 5000 THEN 'Regular'
+		ELSE 'New'
+	END customer_segment
+FROM customer_spending
+*/
+
+-- Creating new dimension by segmentation
+
+SELECT
+	customer_segment,
+	COUNT(customer_key) AS total_customers
+FROM(
+	SELECT
+	customer_key,
+	CASE WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+		WHEN lifespan >= 12 AND total_spending < 5000 THEN 'Regular'
+		ELSE 'New'
+	END customer_segment
+	FROM customer_spending)t
+GROUP BY customer_segment 
+ORDER BY total_customers DESC
